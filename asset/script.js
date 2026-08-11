145f445f9029732de928a64c828f32932b274502
maryam <maryumsultani12@gmail.com>
2026-08-11 20:34:11 +0500

    Updated Repo

---
+++ asset/script.js
@@ -0,0 +1,9 @@
+  // const els = document.querySelectorAll('.reveal');
+  // if ('IntersectionObserver' in window){
+  //   const io = new IntersectionObserver((entries)=>{
+  //     entries.forEach(e=>{ if(e.isIntersecting){ e.target.classList.add('in'); io.unobserve(e.target); } });
+  //   }, {threshold:0.15});
+  //   els.forEach(el=>io.observe(el));
+  // } else {
+  //   els.forEach(el=>el.classList.add('in'));
+  // }
